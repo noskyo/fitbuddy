aa99aa934b6ab33a75fb65600f0476b8d2e13587
dieplian <lian.diep.1@ens.etsmtl.ca>
2024-11-23 19:43:24 -0500

    Merge completed

--- lib/map/MapPage.dart
+++ lib/map/MapPage.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
-import 'mapData.dart';
+import 'MapData.dart';
 import '../NavBar.dart';
 
 class InteractiveMap extends StatefulWidget {
